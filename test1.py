4aa64de3777289f4aaeb8dd548a29682171a079e
chandruBadiger <chandru.badiger@gmail.com>
2022-12-12 18:46:47 +0530

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,7 +1,2 @@
 print("This is to test my project...")
 print("this is another test")
-def test_keyword():
-    """
-    This is a testkeyword for python pylints
-    """
-    print("Sume of a and b is:")
