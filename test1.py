2cd390c66556f018466a1c33ca6401f07ad37d04
chandruBadiger <chandru.badiger@gmail.com>
2022-12-12 18:13:26 +0530

    keyword added

--- test1.py
+++ test1.py
@@ -1,2 +1,11 @@
 print("This is to test my project...")
 print "this is another test"
+@keyword(name="test keyword")
+def test_keyword():
+    """
+    This is a testkeyword for python pylints
+    """
+    a = 10
+    b =20
+    sum = a+ b
+    
