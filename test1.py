d658b1dfb31db1f8c9aaeffef9530f684acab172
chandruBadiger <chandru.badiger@gmail.com>
2023-10-30 15:42:23 +0530

    Update test1.py

--- test1.py
+++ test1.py
@@ -4,3 +4,6 @@ This module is just for testing the pylint errors
 print("This is to test my project...")
 print("this is another test ...")
 print("this is to test jenkins integration test ...")
+print("This is to test my project1...")
+print("this is another test2 ...")
+print("this is to test jenkins integration test3 ...")
